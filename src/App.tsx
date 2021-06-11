6b9fedcfa1891dd61df0e83f4b072dff69e97bed
Hassan Mahmud <hassanmahmud.sa@gmail.com>
2021-06-11 07:42:29 +0600

    Update configurations files.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,21 @@
+import './styles.css'
+import IMAGE from './facebook.jpeg'
+import LOGO from './logo.svg'
+import { ClickCounter } from './ClickCounter'
+
 export const App = () => {
+  //const num = 0;
+  const name = 'Hassan Mahmud'
+
   return (
-    <h3>React, TypeScript and Webpack</h3>
+    <>
+      <h3>
+        React, TypeScript and Webpack - {process.env.NODE_ENV} {name}
+        {process.env.name}
+      </h3>
+      <img src={IMAGE} alt="Facebook logo" width="100" />
+      <img src={LOGO} alt="React Logo" width="100" />
+      <ClickCounter />
+    </>
   )
 }
